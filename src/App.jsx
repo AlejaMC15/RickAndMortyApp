0a7f93e60515206702f98690d4ee01593590e900
amenesesc <amenesesc@indracompany.com>
2022-07-24 17:54:43 -0500

    Create rick and morty app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Home from "./views/home/Home";
+
+function App() {
+  return (
+    <>
+      <header>
+        <span>Rick and Morty app</span>
+      </header>
+      <main>
+        <Home />
+      </main>
+      <footer>
+        <span>Created by Alejandra Meneses Carmona</span>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/views/home/Home.jsx
@@ -0,0 +1,14 @@
+import Search from "../../components/search/Search";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="containerHome">
+      <main>
+        <Search />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
